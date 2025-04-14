4240f139fb932ab0dbdce984972c429162711a8f
othmanech <othmanechougrad12@gmail.com>
2025-04-14 18:48:48 +0100

    Hello blog

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ['PYTHONANYWHERE_API_TOKEN'] = '2bb5ec99228a119f7e0cb8ca269d7284df87bb6a'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
